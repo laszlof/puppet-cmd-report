bd0c8aca19b46f7d0e8d88d2a933d4649334f5f7
Frank Laszlo <flaszlo@nexcess.net>
2016-02-17 16:43:29 -0500

    Final fix

---
+++ README.md
@@ -0,0 +1,12 @@
+This module will allow you to pass your puppet reports to an arbitrary script.
+
+## Usage
+
+Create a file in your puppet config directory called: `cmdreport.yaml` with the following format:
+
+```
+cmd: "/path/to/somecmd --somearg %s --someotherarg"
+replace_char: "%s"
+```
+
+The script will replace the `replace_char` with the path to a temp file containing the report generated from puppet.
--- lib/puppet/reports/cmdreport.rb
+++ lib/puppet/reports/cmdreport.rb
@@ -6,9 +6,13 @@ Puppet::Reports.register_report(:cmdreport) do
   @configfile = File.join([File.dirname(Puppet.settings[:config]), "cmdreport.yaml"])
   raise(Puppet::ParseError, "Cmdreport config file #{@configfile} unreadable!") unless File.exists?(@configfile)
   config = YAML.load_file(@configfile)
-  CMD = config[:cmd]
-  if config.key?(:replace_char) then
-    REPLACE = config[:replace_char]
+  if config.key?('cmd') then
+    CMD = config['cmd']
+  else
+    raise(Puppet::ParseError, 'Missing cmd from cmdreport.xml!')
+  end
+  if config.key?('replace_char') then
+    REPLACE = config['replace_char']
   else
     REPLACE = '%s'
   end
@@ -22,7 +26,7 @@ DESC
     begin
       fp.write(self.to_yaml)
       cmd = CMD.gsub(REPLACE, fp.path)
-      ret = %x(cmd)
+      ret = system(cmd)
     ensure
       fp.close
       fp.unlink
