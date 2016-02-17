20aeed8b31829e2a9d5c31aa1d831ce29e7938d5
Frank Laszlo <flaszlo@nexcess.net>
2016-02-17 15:47:43 -0500

    Initial Commit

---
+++ lib/puppet/reports/cmdreport.rb
@@ -0,0 +1,31 @@
+require 'puppet'
+require 'tempfile'
+require 'yaml'
+
+Puppet::Reports.register_report(:cmdreport) do
+  @configfile = File.join([File.dirname(Puppet.settings[:config]), "cmdreport.yaml"])
+  raise(Puppet::ParseError, "Cmdreport config file #{@configfile} unreadable!") unless File.exists?(@configfile)
+  config = YAML.load_file(@configfile)
+  CMD = config[:cmd]
+  if config.key?(:replace_char) then
+    REPLACE = config[:replace_char]
+  else
+    REPLACE = '%s'
+  end
+
+  desc <<-DESC
+Send reports to command line script
+DESC
+
+  def process
+    fp = Tempfile.new('puppet-nw')
+    begin
+      fp.write(self.to_yaml)
+      cmd = CMD.gsub! REPLACE, fp.path
+      ret = %x(cmd)
+    ensure
+      fp.close
+      fp.unlink
+    end
+  end
+end
